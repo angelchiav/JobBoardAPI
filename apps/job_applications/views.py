2fe57fd48d2c34a18828a9b0286850ff0a75534d
santy <sandresrodriguezmo@gmail.com>
2025-08-20 21:02:56 +0200

    create viewSets for technology and Vacancies

--- apps/job_applications/views.py
+++ apps/job_applications/views.py
@@ -1,3 +1,49 @@
-from django.shortcuts import render
+from rest_framework.response import Response
+from rest_framework.decorators import action
+from rest_framework import viewsets, status
+from .models import Technology, Vacancy, JobApplication, ApplicationStatusHistory, Interview, ApplicationDocument
+from .serializers import TechnologySerializer, VacancySerializer, VacancyListSerializer, VacancyCreateSerializer, JobApplicationSerializer, JobApplicationListSerializer, JobApplicationCreateSerializer, JobApplicationUdpateSerializer
 
-# Create your views here.
+
+# ya hay funciones crud de forma automatica
+class ThechnologyViewSet(viewsets.ModelViewSet):
+    queryset = Technology.objects.all()
+    serializer_class = TechnologySerializer
+
+    # crear tecnologia segun la información del serializer
+    def createTechnology(self, request, *args, **kwargs):
+        serializer = TechnologySerializer(data=request.data)
+
+        if serializer.is_valid:
+            serializer.save()
+            return Response(status=status.HTTP_201_CREATED)
+
+        return Response(
+            {"error": "register failed", "details": serializer.errors},
+            status=status.HTTP_400_BAD_REQUEST
+        )
+
+
+class VacancyViewSet(viewsets.ModelViewSet):
+    queryset = Vacancy.objects.all()
+    serializer_class = VacancySerializer
+    # filterset_fields = ('title') #filtrado por titulo
+
+    # listar vacantes
+    def listVacancy(self, request):
+        queryset = Vacancy.objects.all()
+        serializer = VacancySerializer(queryset, many=True)
+        return Response(serializer.data)
+
+    # crear vacante
+    def createVacancy(self, request, *args, **kwargs):
+        serializer = VacancyCreateSerializer(data=request.data)
+
+        if serializer.is_valid:
+            serializer.save()
+            return Response(status=status.HTTP_201_CREATED)
+
+        return Response(
+            {"error": "register failed", "details": serializer.errors},
+            status=status.HTTP_400_BAD_REQUEST
+        )
--- config/urls.py
+++ config/urls.py
@@ -25,5 +25,6 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
     path('api/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
-    path('', include('apps.accounts.urls'))
+    path('', include('apps.accounts.urls')),
+    path('', include('apps.job_applications.urls')),
 ]
